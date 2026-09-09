7c6de30d797e734127396a932c90bd082ed0d248
unknown <fernandobanda1992@gmail.com>
2026-09-09 17:57:02 -0500

    Updated rules page

--- src/pages/Rules.tsx
+++ src/pages/Rules.tsx
@@ -1,38 +1,38 @@
+import {Button} from "antd";
 import {DocumentIcon} from "../Icons.tsx";
 import {Link} from "react-router-dom";
 
 export const Rules = () => {
   return (
-      <div className={"mt-12 p-4"}>
-        <div className={"mb-4"}>
-          <p className={"text-left font-bold"}>Basic Tutorial Video</p>
+      <div className={"p-4 text-xl align-center"}>
+        <div className={"p-4"}>
+          <p>First time playing? Check the basic rules in this tutorial video:</p>
         </div>
-        <div>
-          <iframe width="560" height="315" src="https://www.youtube.com/embed/eVTXPUF4Oz4?si=4VO-qFCOUJwQOGOr"
-                  title="YouTube video player" frameBorder="0"
+        <div className={"flex justify-center"}>
+          <iframe width="560" height="315" style={{border: "2px solid #1d2188"}}
+                  src="https://www.youtube.com/embed/JSa5DWNFqzQ?rel=0"
+                  title="YouTube video player"
                   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                   referrerPolicy="strict-origin-when-cross-origin"
                   allowFullScreen></iframe>
         </div>
-        <div className={"my-4"}>
-          <p className={"flex items-center gap-2 text-left font-bold"}>
-            <span>Rulebook</span>
-            <a href="https://pdfobject.com/pdf/sample.pdf" target={"_blank"} download="Nombre_Del_Archivo.pdf">
-              <DocumentIcon/>
-            </a>
-          </p>
+        <div className={"mt-10"}>
+          <p className={"mb-2"}>For more advanced rules check these documents:</p>
+          <div className={"p-1 flex flex-row gap-2 justify-center items-center"}>
+            <DocumentIcon/>
+            <span className="font-bold text-lg">Rulebook</span>
+            <Button className={"w-20"} htmlType={"button"} target="_blank" href="https://fbanda.github.io/Arena-MedabotsCard/Assets/MedalClash_Rulebook_v1.pdf">English</Button>
+            <Button className={"w-20"} htmlType={"button"} target="_blank" href="https://fbanda.github.io/Arena-MedabotsCard/Assets/MedalClash_Rulebook_v1_es.pdf">Español</Button>
+          </div>
+          <div className={"p-1 flex flex-row gap-2 justify-center items-center"}>
+            <DocumentIcon/>
+            <span className="font-bold text-lg">Keyword Rulings</span>
+            <Button className={"w-20"} htmlType={"button"} target="_blank" href="https://fbanda.github.io/Arena-MedabotsCard/Assets/MedalClash_KeywordRulings_v1.pdf">English</Button>
+            <Button className={"w-20"} htmlType={"button"} target="_blank" href="https://fbanda.github.io/Arena-MedabotsCard/Assets/MedalClash_KeywordRulings_v1_es.pdf">Español</Button>
+          </div>
         </div>
-        <div className={"my-4"}>
-          <p className={"flex items-center gap-2 text-left font-bold"}>
-            <span>Keyword Rulings</span>
-            <a href="https://pdfobject.com/pdf/sample.pdf" target={"_blank"} download="Nombre_Del_Archivo.pdf">
-              <DocumentIcon/>
-            </a>
-          </p>
-        </div>
-        <div className={"my-4"}>
-          <p className={"text-left"}>
-            <span className={"font-bold me-2"}>Card Ruling:</span>
+        <div className={"mt-5"}>
+          <p className={"mb-2"}>
             <span>
               For individual card rulings, search for the cards <Link className={"underline"} to={"/"}>here</Link> and scroll to the ruling section.
             </span>
